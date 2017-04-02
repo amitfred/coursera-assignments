56c6928af9a5b82bcce6434074428eee0af9118c
amit <amit@something.com>
2017-04-02 20:42:02 +0530

    added code to consider empty items

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -3,34 +3,57 @@
 
   angular.module('LunchCheckerApp',[])
 
-  .controller('LunchCheckerController',function ($scope){
-       $scope.lunchItems="";
-       $scope.message ="";
+  .controller('LunchCheckerController',LunchCheckerController);
 
-       $scope.checker = function functionName() {
-            var message =checkValue($scope.lunchItems);
-            $scope.message =message;
-       };
+  LunchCheckerController.$inject=['$scope'];
+  function LunchCheckerController($scope) {
+    $scope.lunchItems="";
+          $scope.message ="";
 
-       function checkValue(string) {
-            var message ="";
+          $scope.checker = function functionName() {
+               var message =checkValue($scope.lunchItems);
+               $scope.message =message;
+          };
 
-            if(string==""){
-                message = "Please enter data first";
-                return message;
-            }
+          function checkValue(string) {
+               var message ="";
 
-            var items = string.split(',');
-            var length = items.length;
-            console.log(length);
-            if (length<=3) {
-               message = "Enjoy!";
-            }
-            else if (length>3) {
-              message = "Too much!"
-            }
+               if(string==""){
+                   message = "Please enter data first";
+                   return message;
+               }
 
-            return message;
-       }
-}) ;
+               var items = string.split(',');
+
+               var itemsForRemoval = string.split(',');
+               var oldLength = items.length;
+               var length = itemsForRemoval.length;
+
+               for(var i=0; i<oldLength;i++){
+                 if(items[i].trim()==""){
+
+                  length= length-1;
+
+                 }
+               }
+                console.log(itemsForRemoval);
+
+
+
+               console.log(length);
+               if (length>0 && length<=3) {
+                  message = "Enjoy!";
+               }
+               else if (length>3) {
+                 message = "Too much!"
+               }
+               else if(length<=0){
+                 message = "Please enter data first"
+               }
+
+               return message;
+          }
+  }
+
+
 })();
